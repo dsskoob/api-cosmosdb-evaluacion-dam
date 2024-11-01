5e61d931c04ca5454e133aedda21895a8b0832a9
Daniela Ayala <daniam-17@hotmail.com>
2024-11-01 17:30:29 +0000

    proyectos

--- app.py
+++ app.py
@@ -76,3 +76,96 @@ def eliminar_usuario(usuario_id:str):
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     except exceptions.CosmosHttpResponseError as e:
         raise HTTPException(status_code=400, detail= str(e))
+
+
+
+############################# Endpoint de Proyectos #############################
+
+@app.post("/proyectos/", response_model=Proyecto, status_code=201)
+def crear_proyecto(proyecto: Proyecto):
+
+    try:
+        #Obtener usuario
+        proyecto_dict = proyecto.dict()
+        usuario_id = proyecto_dict['id_usuario']
+
+        try:
+            usuario = usuario_container.read_item(item=usuario_id, partition_key=usuario_id)
+        except exceptions.CosmosResourceNotFoundError:
+            raise HTTPException(status_code=404, detail="El usuario no existe")
+
+        # insertar proyecto
+        proyecto_container.create_item(body=proyecto_dict)
+
+        return proyecto_dict
+    except exceptions.CosmosResourceExistsError:
+        raise HTTPException(status_code=400, detail="El proyecto ya existe")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get('/proyectos/', response_model=List[Proyecto])
+def obtener_lista_proyectos():
+    try:
+        script = 'select * from c WHERE 1 = 1'
+        proyectos_list = list(proyecto_container.query_items(query=script,enable_cross_partition_query=True))
+        return proyectos_list
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="No contiene proyectos")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail= str(e))
+ 
+
+@app.get('/usuarios/{usuario_id}/proyectos/', response_model=List[Proyecto])
+def obtener_lista_proyectos_usuario(usuario_id: str):
+    try:
+        script = f"SELECT * FROM c WHERE c.id_usuario = '{usuario_id}'"
+        proyectos_list = list(proyecto_container.query_items(query=script, enable_cross_partition_query=True))
+
+        if not proyectos_list:
+            raise HTTPException(status_code=404, detail="No contiene proyectos")
+        
+        return proyectos_list
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="No contiene proyectos")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+ 
+
+@app.put("/proyectos/{proyecto_id}", response_model=Proyecto)
+def actualiza_proyecto(proyecto_id: str, updated_proyecto: Proyecto):
+    try:
+        #Obtener usuario
+        updated_proyecto_dict = updated_proyecto.dict()
+        usuario_id = updated_proyecto_dict['id_usuario']
+
+        try:
+            usuario = usuario_container.read_item(item=usuario_id, partition_key=usuario_id)
+        except exceptions.CosmosResourceNotFoundError:
+            raise HTTPException(status_code=404, detail="El usuario no existe")
+
+        #Obtiene proyecto
+        existing_proyecto = proyecto_container.read_item(item=proyecto_id, partition_key=proyecto_id)
+
+        existing_proyecto.update(updated_proyecto.dict(exclude_unset=True))
+        existing_proyecto['id'] = proyecto_id
+
+        proyecto_container.replace_item(item=proyecto_id, body=existing_proyecto)
+ 
+        return existing_proyecto
+        
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='Proyecto no encotrado')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+ 
+@app.delete("/proyectos/{proyecto_id}", status_code=204)
+def eliminar_proyecto(proyecto_id: str):
+    try:
+        proyecto_container.delete_item(item=proyecto_id, partition_key=proyecto_id)
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="Proyecto no encontrado")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail= str(e))
